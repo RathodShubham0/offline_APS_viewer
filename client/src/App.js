5bdb08bbfadba152cc7fb258c0853247d7fb2f1f
Rathodshubham <shubham.21910641@viit.ac.in>
2025-01-13 10:50:29 +0530

    service worker enabled

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Viewer from './Viewer/Viewer';
+import DownloadSVF from './components/DownloadSVF';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -12,10 +13,12 @@ class App extends React.Component {
      
     return (
       <div className="app">
+        {/* <DownloadSVF/> */}
         <div style={{ position: 'relative', width: '100%', height: '100%' }}>
           <Viewer
             ref={ref => this.wrapper = ref}
           />
+
         </div>
       </div>
     );
