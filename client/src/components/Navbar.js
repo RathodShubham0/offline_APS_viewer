f52c50abcaf7fd64abcfcc2dbce45462d56930ec
RathodShubham0 <shubham.rathod@cctech.co.in>
2025-01-16 19:13:33 +0530

    navbar added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,8 +11,11 @@ export function Navbar({handleToggle}){
        <nav className="navbar">
         <h4 className=" min-letter-spacing pl-4" >APS Viewer</h4>
         <div className="toggle-container">
+           <li>
+             <a href="/ " className='p-2'>Home</a>
+              <a href="/dashboard">Dashboard</a>
+            </li>
           <button  id="status"  onClick={()=>{handleToggle();   }}    >Model: Online </button>
-
           <SvfDownloader/>
         </div>
       </nav>
