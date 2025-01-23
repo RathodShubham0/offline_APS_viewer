485c6e15100e7ed3e844e61da7bfb80457432133
RathodShubham0 <shubham.rathod@cctech.co.in>
2025-01-23 19:09:33 +0530

    nav bar path change

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,8 +12,8 @@ export function Navbar({handleToggle}){
         <h4 className=" min-letter-spacing pl-4" >APS Viewer</h4>
         <div className="toggle-container">
            <li>
-             <a href="/ " className='p-2'>Home</a>
-              <a href="/dashboard">Dashboard</a>
+            
+              <a style={{color: 'white'}}  className=" min-letter-spacing pr-4" href="/">Dashboard</a>
             </li>
           <button  id="status"  onClick={()=>{handleToggle();   }}    >Model: Online </button>
           
