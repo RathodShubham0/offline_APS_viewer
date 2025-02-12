90d3e65646f1d096c3cd6c6a88bb3e8a5196e52b
RathodShubham0 <shubham.rathod@cctech.co.in>
2025-02-12 16:26:14 +0530

    sc

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Viewer from './Viewer/Viewer';
 import { Navbar } from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+ //sc
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -26,8 +26,9 @@ class App extends React.Component {
         <Navbar handleToggle={this.handleToggle} />
       </div>
           <Routes>
-            <Route path="/" element={<Viewer ref={ref => this.wrapper = ref} style={{ position: 'relative', width: '100%', height: '90vh' }} />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            {/* <Route path="/" element={<Viewer ref={ref => this.wrapper = ref} style={{ position: 'relative', width: '100%', height: '90vh' }} />} /> */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/viewer/:modelUrn" element={<Viewer ref={ref => this.wrapper = ref} />} />
           </Routes>
         </div>
       </Router>
